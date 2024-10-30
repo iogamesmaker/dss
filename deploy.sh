f4d360775d928e59fc688558f352ef6d3bf77c06
iogamesplayer <iogamesplayermc@gmail.com>
2024-10-30 10:58:37 +0100

    Deploy to GH pages

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+git add .
+git commit -m "Deploy to GH pages"
+
+git push -u origin main --force
+
--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp8nmqj13c.js
+// include: /tmp/tmp8rzwtji0.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -230,21 +230,21 @@ Module['FS_createPath']("/assets/sprite", "stars", true, true);
 
   })();
 
-// end include: /tmp/tmp8nmqj13c.js
-// include: /tmp/tmpcgoiv88j.js
+// end include: /tmp/tmp8rzwtji0.js
+// include: /tmp/tmp_pmjeog7.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpcgoiv88j.js
-// include: /tmp/tmpoqk2akjo.js
+  // end include: /tmp/tmp_pmjeog7.js
+// include: /tmp/tmpyca0hv_t.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpoqk2akjo.js
+  // end include: /tmp/tmpyca0hv_t.js
 
 
 // Sometimes an existing Module object exists with properties
